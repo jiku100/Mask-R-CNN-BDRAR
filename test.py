e00f5bd4246fbe9bbaac2d9664b144cecfa593ae
ZEND <60598554+jiku100@users.noreply.github.com>
2021-05-22 23:46:14 +0900

    first commit

---
+++ test.py
@@ -0,0 +1,265 @@
+import os
+import time
+import numpy as np
+
+# Download and install the Python COCO tools from https://github.com/waleedka/coco
+# That's a fork from the original https://github.com/pdollar/coco with a bug
+# fix for Python 3.
+# I submitted a pull request https://github.com/cocodataset/cocoapi/pull/50
+# If the PR is merged then use the original repo.
+# Note: Edit PythonAPI/Makefile and replace "python" with "python3".
+from pycocotools.coco import COCO
+from pycocotools.cocoeval import COCOeval
+from pycocotools import mask as maskUtils
+
+import zipfile
+import urllib.request
+import shutil
+
+from config import Config
+import utils
+import model as modellib
+import cv2
+import torch
+
+ROOT_DIR = os.getcwd()
+
+# Path to trained weights file
+COCO_MODEL_PATH = os.path.join(ROOT_DIR, "mask_rcnn_coco.pth")
+
+# Directory to save logs and model checkpoints, if not provided
+# through the command line argument --logs
+DEFAULT_LOGS_DIR = os.path.join(ROOT_DIR, "logs")
+DEFAULT_DATASET_YEAR = "2019"
+
+class CocoConfig(Config):
+    """Configuration for training on MS COCO.
+    Derives from the base Config class and overrides values specific
+    to the COCO dataset.
+    """
+    # Give the configuration a recognizable name
+    NAME = "coco"
+
+    # We use one GPU with 8GB memory, which can fit one image.
+    # Adjust down if you use a smaller GPU.
+    IMAGES_PER_GPU = 16
+
+    # Uncomment to train on 8 GPUs (default is 1)
+    # GPU_COUNT = 8
+
+    # Number of classes (including background)
+    NUM_CLASSES = 1 + 2
+
+class CocoDataset(utils.Dataset):
+    def load_coco(self, dataset_dir, subset):
+
+        coco = COCO("{}/annotations/SOBA_{}.json".format(dataset_dir, subset))
+        image_dir = "{}/{}".format(dataset_dir, "SOBA")
+        class_ids = sorted(coco.getCatIds())
+        image_ids = list(coco.imgs.keys())
+        # Add classes
+        for i in class_ids:
+            self.add_class("SOBA", i, coco.loadCats(i)[0]["name"])
+
+        # Add images
+        for i in image_ids:
+            self.add_image(
+                "SOBA", image_id=i,
+                path=os.path.join(image_dir, coco.imgs[i]['file_name']),
+                width=coco.imgs[i]["width"],
+                height=coco.imgs[i]["height"],
+                annotations=coco.loadAnns(coco.getAnnIds(
+                    imgIds=[i], catIds=class_ids, iscrowd=None)))
+        
+        return coco
+    
+    def load_mask(self, image_id):
+        # If not a COCO image, delegate to parent class.
+        image_info = self.image_info[image_id]
+        if image_info["source"] != "SOBA":
+            return super(CocoDataset, self).load_mask(image_id)
+
+        instance_masks = []
+        class_ids = []
+        annotations = self.image_info[image_id]["annotations"]
+        i = 0
+        for annotation in annotations:
+            class_id = self.map_source_class_id(
+                "SOBA.{}".format(annotation['category_id']))
+            if class_id:
+                m = self.annToMask(annotation, image_info["height"],
+                                   image_info["width"])
+
+           
+                if m.max() < 1:
+                    continue
+                # Is it a crowd? If so, use a negative class ID.
+                if annotation['iscrowd']:
+                    # Use negative class ID for crowds
+                    class_id *= -1
+                    # For crowd masks, annToMask() sometimes returns a mask
+                    # smaller than the given dimensions. If so, resize it.
+                    if m.shape[0] != image_info["height"] or m.shape[1] != image_info["width"]:
+                        m = np.ones([image_info["height"], image_info["width"]], dtype=bool)
+                instance_masks.append(m)
+                class_ids.append(class_id)
+
+        # Pack instance masks into an array
+        if class_ids:
+            mask = np.stack(instance_masks, axis=2)
+            class_ids = np.array(class_ids, dtype=np.int32)
+            return mask, class_ids
+        else:
+            # Call super class to return an empty mask
+            return super(CocoDataset, self).load_mask(image_id)
+    def annToRLE(self, ann, height, width):
+        """
+        Convert annotation which can be polygons, uncompressed RLE to RLE.
+        :return: binary mask (numpy 2D array)
+        """
+        segm = ann['segmentation']
+        if isinstance(segm, list):
+            # polygon -- a single object might consist of multiple parts
+            # we merge all parts into one mask rle code
+            rles = maskUtils.frPyObjects(segm, height, width)
+            rle = maskUtils.merge(rles)
+        elif isinstance(segm['counts'], list):
+            # uncompressed RLE
+            rle = maskUtils.frPyObjects(segm, height, width)
+        else:
+            # rle
+            rle = ann['segmentation']
+        return rle
+
+    def annToMask(self, ann, height, width):
+        """
+        Convert annotation which can be polygons, uncompressed RLE, or RLE to binary mask.
+        :return: binary mask (numpy 2D array)
+        """
+        rle = self.annToRLE(ann, height, width)
+        m = maskUtils.decode(rle)
+        return m
+
+############################################################
+#  Training
+############################################################
+
+
+if __name__ == '__main__':
+    import argparse
+
+    # Parse command line arguments
+    parser = argparse.ArgumentParser(
+        description='Train Mask R-CNN on SOBA.')
+    parser.add_argument("command",
+                        metavar="<command>",
+                        help="'train' or 'evaluate' on MS COCO")
+    parser.add_argument('--dataset', required=True,
+                        metavar="/path/to/coco/",
+                        help='Directory of the MS-COCO dataset')
+    parser.add_argument('--model', required=False,
+                        metavar="/path/to/weights.pth",
+                        help="Path to weights .pth file or 'coco'")
+    parser.add_argument('--logs', required=False,
+                        default=DEFAULT_LOGS_DIR,
+                        metavar="/path/to/logs/",
+                        help='Logs and checkpoints directory (default=logs/)')
+
+    args = parser.parse_args()
+    print("Command: ", args.command)
+    print("Model: ", args.model)
+    print("Dataset: ", args.dataset)
+    print("Logs: ", args.logs)
+
+    # Configurations
+    if args.command == "train":
+        config = CocoConfig()
+    else:
+        class InferenceConfig(CocoConfig):
+            # Set batch size to 1 since we'll be running inference on
+            # one image at a time. Batch size = GPU_COUNT * IMAGES_PER_GPU
+            GPU_COUNT = 1
+            IMAGES_PER_GPU = 1
+            DETECTION_MIN_CONFIDENCE = 0
+        config = InferenceConfig()
+    config.display()
+
+    # Create model
+    if args.command == "train":
+        model = modellib.MaskRCNN(config=config,
+                                  model_dir=args.logs)
+    else:
+        model = modellib.MaskRCNN(config=config,
+                                  model_dir=args.logs)
+    if config.GPU_COUNT:
+        model = model.cuda()
+
+    # Select weights file to load
+    if args.model:
+        if args.model.lower() == "coco":
+            model_path = COCO_MODEL_PATH
+        elif args.model.lower() == "last":
+            # Find last trained weights
+            model_path = model.find_last()[1]
+        elif args.model.lower() == "imagenet":
+            # Start from ImageNet trained weights
+            model_path = config.IMAGENET_MODEL_PATH
+        else:
+            model_path = args.model
+    else:
+        model_path = ""
+
+    # Load weights
+    print("Loading weights ", model_path)
+    model.load_weights(model_path)
+
+    # Train or evaluate
+    if args.command == "train":
+        # Training dataset. Use the training set and 35K from the
+        # validation set, as as in the Mask RCNN paper.
+        dataset_train = CocoDataset()
+        dataset_train.load_coco(args.dataset, "train")
+        dataset_train.load_coco(args.dataset, "val")
+        dataset_train.prepare()
+
+        # Validation dataset
+        dataset_val = CocoDataset()
+        dataset_val.load_coco(args.dataset, "val")
+        dataset_val.prepare()
+
+    #     # *** This training schedule is an example. Update to your needs ***
+
+        # Training - Stage 1
+        print("Training network heads")
+        model.train_model(dataset_train, dataset_val,
+                    learning_rate=config.LEARNING_RATE,
+                    epochs=40,
+                    layers='heads')
+
+    #     # Training - Stage 2
+    #     # Finetune layers from ResNet stage 4 and up
+    #     print("Fine tune Resnet stage 4 and up")
+    #     model.train_model(dataset_train, dataset_val,
+    #                 learning_rate=config.LEARNING_RATE,
+    #                 epochs=120,
+    #                 layers='4+')
+
+    #     # Training - Stage 3
+    #     # Fine tune all layers
+    #     print("Fine tune all layers")
+    #     model.train_model(dataset_train, dataset_val,
+    #                 learning_rate=config.LEARNING_RATE / 10,
+    #                 epochs=160,
+    #                 layers='all')
+
+    # elif args.command == "evaluate":
+    #     # Validation dataset
+    #     dataset_val = CocoDataset()
+    #     coco = dataset_val.load_coco(args.dataset, "minival", year=args.year, return_coco=True, auto_download=args.download)
+    #     dataset_val.prepare()
+    #     print("Running COCO evaluation on {} images.".format(args.limit))
+    #     evaluate_coco(model, dataset_val, coco, "bbox", limit=int(args.limit))
+    #     evaluate_coco(model, dataset_val, coco, "segm", limit=int(args.limit))
+    # else:
+    #     print("'{}' is not recognized. "
+    #           "Use 'train' or 'evaluate'".format(args.command))
